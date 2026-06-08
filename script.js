66014f2b6b69f16b35b5bd966ef4d57b759249a9
seldevmh-boop <seldevmh@gmail.com>
2026-06-08 20:07:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,160 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry1');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry2');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry3');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry4');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry5');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry6');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry7');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry8');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry9');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
+document.addEventListener('DOMContentLoaded', function () {
+    var link = document.getElementById('sorry10');
+    var msg = document.getElementById('msg');
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        msg.style.display = 'block';
+        var link2 = document.getElementById('sorry-none');
+        link2.addEventListener('click', function (e) {
+            e.preventDefault();
+            msg.style.display = 'none';
+
+        })
+    })
+}
+
+)
